845d94cc1a083f1c92e1b844f26c62891afd23b2
Nick <nhgood@buffalo.edu>
2021-02-26 22:16:09 -0500

    tutorial 1 is done

--- server/index.js
+++ server/index.js
@@ -2,14 +2,25 @@ const express = require('express');
 const ejs = require ('ejs');
 const path = require('path');
 
-const clientPath = path.join(__dirname, '../client')
-const staticPath = path.join(clientPath,'/static');
-const viewsPath = path.join(clientPath, '/views')
+const clientPath = path.join(__dirname, '../client/')
+const staticPath = path.join(clientPath,'/static/');
+const viewsPath = path.join(clientPath, '/views/')
 
 const app = express(); 
 
 app.set('view engine','ejs');
 app.set('views',viewsPath);
+
+var x = 0;
+
+const counter = function(req, res, next) {
+	x++
+	console.log(x);
+	next();
+}
+
+app.use(counter);
+
 
 app.get('/', function(req, res) {
 	res.render('index');
