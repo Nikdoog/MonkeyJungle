a0d003fd12f550059f83cebdefa0de1b8e729c68
Nick <nhgood@buffalo.edu>
2021-03-15 04:00:59 -0400

    Tutorial 2 and 3

--- server/index.js
+++ server/index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const ejs = require ('ejs');
 const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const clientPath = path.join(__dirname, '../client/')
 const staticPath = path.join(clientPath,'/static/');
 const viewsPath = path.join(clientPath, '/views/')
 
 const app = express(); 
+app.use(express.static(staticPath));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+	name: 'monkeytime',
+	secret: 'whoopinggibbonmeme',
+	saveUninitialized: false,
+	resave: false,
+	cookie: {
+	    maxAge: 1000*60*60*24*3,
+	}
+}));
+
+app.listen(2000);
+
 
 app.set('view engine','ejs');
 app.set('views',viewsPath);
@@ -22,14 +38,18 @@ const counter = function(req, res, next) {
 app.use(counter);
 
 
+
 app.get('/', function(req, res) {
-	res.render('index');
+	res.render('index', {data: req.session});
 });
 
 app.get('/famous', function(req, res) {
-	res.render('famous');
+	res.render('famous', {data: req.session});
 }); 
 
-app.use(express.static(staticPath))
+app.post('/welcome', (req, res) => {
+	console.log(req.body);
+	req.session.username=req.body.nombre;
+	res.send('SUCCESS');
+});
 
-app.listen(2000);
